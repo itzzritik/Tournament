a1486667f8451af9894abea7092a2121ac49ed30
Ritik Srivastava <ritik.space@gmail.com>
2023-12-21 01:46:15 +0530

    📦 Setup Dependencies and Lint

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	eslint: {
+        ignoreDuringBuilds: true,
+    },
+
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: [
+				{
+					loader: "@svgr/webpack",
+					options: {
+						typescript: true,
+						ext: "tsx",
+					},
+				},
+			],
+		});
+
+		return config;
+	},
+};
+
+module.exports = nextConfig;
